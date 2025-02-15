1dd37e9d4137f121c2e0c739ca3ea4999fc2c4fd
K2 <ktwo@ktwo.ca>
2025-02-14 23:14:29 -0500

    Not working yet, next re-org of code

---
+++ root_service.py
@@ -0,0 +1,9 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = []
+# ///
+from dependency_injector import inject, Provide, providers
+from dependency_injector.wiring import Provide, inject
+from nodes_config import nodes_config
+from nodes_injector import Container
+
